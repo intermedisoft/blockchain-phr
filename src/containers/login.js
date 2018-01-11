87435380371ca4b7be1f4488727c717720f45547
Kritsada Lawan <kritsada@hospital-os.com>
2018-01-11 09:16:58 +0700

    change menu

--- src/containers/login.js
+++ src/containers/login.js
@@ -7,6 +7,7 @@ import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 import Loadable from 'react-loading-overlay'
 import logo from '../assets/images/logo.svg'
 import styles from '../assets/style/themes/pages/login.scss'
+// import Button from 'material-ui/Button'
 
 const LoginPage = ({ firebase, auth }) => {
   if (!isLoaded(auth)) {

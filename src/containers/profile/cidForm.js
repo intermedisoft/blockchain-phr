7c518e424a50de2ba8f1c102c9a4beb5edae6511
arechainut <dektaiimage@gmail.com>
2018-01-15 17:44:42 +0700

    Add style

--- src/containers/profile/cidForm.js
+++ src/containers/profile/cidForm.js
@@ -3,17 +3,19 @@ import { Field, reduxForm } from 'redux-form'
 import Button from 'material-ui/Button'
 import { Input } from './../../components'
 
+import styles from '../../assets/style/themes/pages/cid.scss'
+
 const CidForm = props => {
   const { handleSubmit } = props
   return (
     <form noValidate autoComplete='off' onSubmit={handleSubmit}>
       {/* <div>เลขที่บัตรประชาชน</div> */}
       <div>
-        <Field component={Input} type={'number'} label={'เลขที่บัตรประชาชน'} name={'cid'} fullWidth={'true'} className={'center widthMax'} />
+        <Field component={Input} type={'number'} label={'เลขที่บัตรประชาชน'} name={'cid'} fullWidth={'true'} maxLength={'13'} className={'center widthMax'} />
       </div>
       <div>
         {/* <button>ลงทะเบียน</button> */}
-        <Button raised type='submit' className={'button'} fullWidth={'true'}>ลงทะเบียน</Button>
+        <Button raised color="primary" type='submit' className={`${styles.btn}`}>ลงทะเบียน</Button>
       </div>
     </form>
   )

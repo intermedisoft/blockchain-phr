d548bcd2970c1c78438ba24bc9f45cb7f23cf24c
arechainut <dektaiimage@gmail.com>
2018-02-02 16:32:39 +0700

    Fix style

--- src/containers/notification/components/list.js
+++ src/containers/notification/components/list.js
@@ -5,6 +5,7 @@ import { List, ListItem } from 'material-ui/List'
 import Divider from 'material-ui/Divider'
 import ActionInfo from 'material-ui/svg-icons/action/info'
 import { Link } from 'react-router-dom'
+import {, blue500} from 'material-ui/styles/colors';
 
 require('moment/locale/th')
 
@@ -12,6 +13,8 @@ class ListComponent extends Component {
   render() {
     const { val } = { ...this.props }
     const data = val.item
+    const iconList = (<ActionInfo color={blue500}/>)
+        
     if (data) {
       return (
         <List>
@@ -31,8 +34,8 @@ class ListComponent extends Component {
               }
               rightIcon={
                 val.type === 'CheckupHistory'
-                  ? data.checkupHistory.patientAcknowledgeDateTime ? null : <ActionInfo />
-                  : data.patientAcknowledgeDateTime ? null : <ActionInfo />
+                  ? data.checkupHistory.patientAcknowledgeDateTime ? null : iconList
+                  : data.patientAcknowledgeDateTime ? null : iconList
               }
               secondaryTextLines={2}
             />
